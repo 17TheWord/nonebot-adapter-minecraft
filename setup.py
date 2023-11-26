41bc2f3a6e56822f781fbfe655c24882865b76a1
17TheWord <17theword@gmail.com>
2023-11-26 23:16:32 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="nonebot-adapter-minecraft",  # 项目名称，保证它的唯一性，不要跟已存在的包名冲突即可
-    version="1.0.1",  # 程序版本
+    version="1.0.2",  # 程序版本
     author="17TheWord",  # 项目作者
     author_email="17theword@gmail.com",  # 作者邮件
     description="NoneBot2与MineCraft Server互通的适配器",  # 项目的一句话描述
