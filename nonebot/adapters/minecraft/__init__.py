340af3c93a12d9ce47469df679e2f95b9bd7b0fe
17TheWord <17theword@gmail.com>
2024-03-10 14:17:21 +0800

    🎨 Update import method

--- nonebot/adapters/minecraft/__init__.py
+++ nonebot/adapters/minecraft/__init__.py
@@ -1,5 +1,8 @@
-from .bot import Bot
-from .event import *
-from .model import *
-from .adapter import Adapter
-from .message import Message, MessageSegment
+from .event import *  # noqa: F403
+from .bot import Bot as Bot
+from .utils import log as log
+from .adapter import Adapter as Adapter
+from .message import Message as Message
+from .message import MessageSegment as MessageSegment
+from .exception import UnauthorizedException as UnauthorizedException
+from .exception import MinecraftAdapterException as MinecraftAdapterException
