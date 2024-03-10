dfa5e0861a25b4303fa21b2b04955d1b036a16a9
17TheWord <17theword@gmail.com>
2024-03-10 14:17:40 +0800

    🎨 Update Config Model

--- nonebot/adapters/minecraft/config.py
+++ nonebot/adapters/minecraft/config.py
@@ -1,12 +1,12 @@
-from typing import Dict, Optional
+from typing import Dict
 
 from pydantic import BaseModel, Field
 
 
 class Server(BaseModel):
-    enable_rcon: Optional[bool] = False
-    rcon_port: Optional[int] = 25575
-    rcon_password: Optional[str] = "password"
+    enable_rcon: bool = False
+    rcon_port: int = 25575
+    rcon_password: str = "password"
 
 
 class Config(BaseModel):
