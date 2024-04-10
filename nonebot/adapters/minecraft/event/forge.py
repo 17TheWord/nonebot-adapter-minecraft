ccfdc877b53be6284170b5d3ccf60a938a316a03
17TheWord <17theword@gmail.com>
2024-04-10 15:01:16 +0800

    ✨ App more player property

--- nonebot/adapters/minecraft/event/forge.py
+++ nonebot/adapters/minecraft/event/forge.py
@@ -14,32 +14,44 @@ class Player(BasePlayer):
     """Forge Player"""
 
     uuid: Optional[str] = None
+    display_name: Optional[str] = None
     ipAddress: Optional[str] = None
-    level: Optional[str] = None
-    """地图？"""
+
     speed: Optional[float] = None
+    flying_speed: Optional[float] = None
+
+    is_flying: Optional[bool] = None
+    is_swimming: Optional[bool] = None
+    is_sleeping: Optional[bool] = None
+    is_blocking: Optional[bool] = None
+
+    game_mode: Optional[str] = None
+
+    block_x: Optional[int] = None
+    block_y: Optional[int] = None
+    block_z: Optional[int] = None
 
 
 class ServerChatEvent(BaseChatEvent):
-    event_name: Literal["ForgeServerChatEvent"]
+    event_name: Literal["ServerChatEvent"]
     player: Player
 
 
 class PlayerLoggedInEvent(BaseJoinEvent):
-    event_name: Literal["ForgePlayerLoggedInEvent"]
+    event_name: Literal["PlayerLoggedInEvent"]
     player: Player
 
 
 class PlayerLoggedOutEvent(BaseQuitEvent):
-    event_name: Literal["ForgePlayerLoggedOutEvent"]
+    event_name: Literal["PlayerLoggedOutEvent"]
     player: Player
 
 
 class PlayerDeathEvent(BaseDeathEvent):
-    event_name: Literal["ForgePlayerDeathEvent"]
+    event_name: Literal["PlayerDeathEvent"]
     player: Player
 
 
 class PlayerCommandEvent(BasePlayerCommandEvent):
-    event_name: Literal["ForgeCommandEvent"]
+    event_name: Literal["CommandEvent"]
     player: Player
