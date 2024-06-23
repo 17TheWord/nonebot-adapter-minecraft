e214c22de5fc0788abdd58c5e84f6c7e4e264543
17TheWord <17theword@gmail.com>
2024-06-23 18:40:45 +0800

    ⚡️ Update Minecraft bot API with new send methods and title message improvements

--- nonebot/adapters/minecraft/bot.pyi
+++ nonebot/adapters/minecraft/bot.pyi
@@ -6,36 +6,27 @@ from .event import Event, MessageEvent
 from .message import Message, MessageSegment
 
 def _check_nickname(bot: Bot, event: MessageEvent): ...
-
-
 async def send(
-        bot: "Bot", event: Event, message: Union[str, Message, MessageSegment], **kwargs
+    bot: "Bot", event: Event, message: Union[str, Message, MessageSegment], **kwargs
 ) -> Any: ...
-
 
 class Bot(BaseBot):
     async def call_api(self, api: str, **data: Any) -> Any: ...
-
     async def handle_event(self, event: Event) -> None: ...
-
     async def send(
-            self, event: Event, message: Union[str, Message, MessageSegment], **kwargs
+        self, event: Event, message: Union[str, Message, MessageSegment], **kwargs
     ) -> Any: ...
-
     async def send_msg(self, message: Union[str, Message, MessageSegment]) -> Any: ...
-
     async def send_actionbar(
-            self, message: Union[str, Message, MessageSegment], **kwargs
+        self, message: Union[str, Message, MessageSegment], **kwargs
     ) -> Any: ...
-
     async def send_title(
-            self,
-            title: str,
-            subtitle: Optional[str] = None,
-            fadein: Optional[int] = 10,
-            stay: Optional[int] = 70,
-            fadeout: Optional[int] = 20,
-            **kwargs,
+        self,
+        title: Union[str, Message, MessageSegment],
+        subtitle: Optional[Union[str, Message, MessageSegment]] = None,
+        fadein: Optional[int] = 10,
+        stay: Optional[int] = 70,
+        fadeout: Optional[int] = 20,
+        **kwargs,
     ) -> Any: ...
-
     async def send_rcon_cmd(self, command: str) -> Tuple[str, int]: ...
