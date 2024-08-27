7da8ab25bff15103c8e7fe8f2831aa2cb65f1e28
17TheWord <17theword@gmail.com>
2024-08-28 00:11:10 +0800

    refactor(utils): 移除get_msg函数并添加数据清理函数移除了get_msg函数，因为它不再使用或需要。同时，添加了两个新函数用于清理数据字典，这些函数排除了None值和False值的键值对。这有助于在处理消息数据时确保数据的整洁和一致性。

--- nonebot/adapters/minecraft/utils.py
+++ nonebot/adapters/minecraft/utils.py
@@ -1,21 +1,13 @@
-from typing import Union
+from typing import Dict, Any
 
 from nonebot.utils import logger_wrapper
-
-from .message import Message, MessageSegment
 
 log = logger_wrapper("Minecraft")
 
 
-def get_msg(message: Union[str, Message, MessageSegment]):
-    message_list = []
-    if isinstance(message, str):
-        message_list.append(MessageSegment.text(message).data)
-    elif isinstance(message, MessageSegment):
-        message_list.append(message.data)
-    elif isinstance(message, Message):
-        for msg in message:
-            message_list.append(msg.data)
-    else:
-        return None
-    return message_list
+def exclude_all_none(data: Dict[str, Any]) -> Dict[str, Any]:
+    return {k: v for k, v in data.items() if v}
+
+
+def exclude_none_and_false(data: Dict[str, Any]) -> Dict[str, Any]:
+    return {k: v for k, v in data.items() if v not in [None, False]}
