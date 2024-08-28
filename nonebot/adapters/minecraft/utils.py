e40041f057da1835032ff1cea749908e5f401638
17TheWord <17theword@gmail.com>
2024-08-28 23:44:33 +0800

    :fire: 移除清理字典函数，添加自定义Json格式化方式，添加API请求返回值处理函数

--- nonebot/adapters/minecraft/utils.py
+++ nonebot/adapters/minecraft/utils.py
@@ -1,13 +1,37 @@
-from typing import Any, Dict
+from enum import Enum
+from typing import Any, Dict, Optional
+from uuid import UUID
+from nonebot.utils import logger_wrapper, DataclassEncoder as BaseDataclassEncoder
 
-from nonebot.utils import logger_wrapper
+from .exception import ActionFailed
 
 log = logger_wrapper("Minecraft")
 
 
-def exclude_all_none(data: Dict[str, Any]) -> Dict[str, Any]:
-    return {k: v for k, v in data.items() if v}
+class DataclassEncoder(BaseDataclassEncoder):
+    """继承并扩展 DataclassEncoder，添加对 UUID 的处理"""
+
+    def default(self, o):
+        if isinstance(o, UUID):
+            return str(o)
+        elif isinstance(o, Enum):
+            return o.value
+        return super().default(o)
 
 
-def exclude_none_and_false(data: Dict[str, Any]) -> Dict[str, Any]:
-    return {k: v for k, v in data.items() if v not in [None, False]}
+def handle_api_result(result: Optional[Dict[str, Any]]) -> Any:
+    """处理 API 请求返回值。
+
+    参数:
+        result: API 返回数据
+
+    返回:
+        API 调用返回数据
+
+    异常:
+        ActionFailed: API 调用失败
+    """
+    if isinstance(result, dict):
+        if result.get("status") == "FAILED":
+            raise ActionFailed(**result)
+        return result.get("data")
