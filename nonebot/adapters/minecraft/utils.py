47c51f4bb558925dd97f4dd221cbdb07ffefeecc
17TheWord <17theword@gmail.com>
2025-10-19 23:01:20 +0800

    :art: 调整工具包相关格式

--- nonebot/adapters/minecraft/utils.py
+++ nonebot/adapters/minecraft/utils.py
@@ -3,9 +3,6 @@ from functools import partial
 from typing import Any, Concatenate, Generic, TypeVar, overload
 from uuid import UUID
 
-from pydantic import BaseModel
-
-from nonebot.compat import PYDANTIC_V2
 from nonebot.utils import DataclassEncoder as BaseDataclassEncoder
 from nonebot.utils import logger_wrapper
 from typing_extensions import ParamSpec
@@ -65,38 +62,16 @@ class DataclassEncoder(BaseDataclassEncoder):
         return super().default(o)
 
 
-def zip_dict(data: BaseModel | dict[str, Any]) -> dict[str, Any]:
-    """
-    将字典中的空值去除
-    :param data: 字典数据
-    :return: 处理后的字典数据
-    """
-    temp_dict = {}
-    if isinstance(data, BaseModel):
-        data = data.model_dump() if PYDANTIC_V2 else data.dict()
-    else:
-        data = data.copy()
-    for k, v in data.items():
-        if v:
-            if isinstance(v, dict):
-                temp_dict[k] = zip_dict(v)
-            elif isinstance(v, list):
-                temp_dict[k] = [zip_dict(i) for i in v]
-            else:
-                temp_dict[k] = v
-    return temp_dict
-
-
 def handle_api_result(result: dict[str, Any] | None) -> Any:
     """处理 API 请求返回值。
 
-    参数:
+    Args:
         result: API 返回数据
 
-    返回:
+    Returns:
         API 调用返回数据
 
-    异常:
+    Raises:
         ActionFailed: API 调用失败
     """
     if isinstance(result, dict):
